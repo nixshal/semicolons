049c6d9182088e6b370cab022a915af6d70fefb6
Nixshal <nixshal@gmail.com>
2024-08-02 17:44:18 +0800

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from io import BytesIO
 # Function to convert an Excel file to CSV with semicolon delimiter
 def convert_excel_to_csv(excel_file):
     excel_data = pd.read_excel(excel_file)
+    excel_data.dropna(how="all", inplace=True)  # Remove empty rows
     csv_buffer = BytesIO()
     excel_data.to_csv(csv_buffer, sep=";", index=False)
     csv_buffer.seek(0)
@@ -14,23 +15,40 @@ def convert_excel_to_csv(excel_file):
 
 
 # Streamlit app
-st.title("Excel to CSV Converter")
+st.subheader("Excel to CSV (semicolon delimiter)")
 
-# File uploader for multiple files
-uploaded_files = st.file_uploader(
-    "Upload Excel files", type="xlsx", accept_multiple_files=True
-)
+# File uploader for a single file
+uploaded_file = st.file_uploader(label="Upload Excel file", type="xlsx")
 
-if uploaded_files:
-    for uploaded_file in uploaded_files:
-        csv_buffer = convert_excel_to_csv(uploaded_file)
-        csv_filename = os.path.splitext(uploaded_file.name)[0] + ".csv"
-        st.download_button(
-            label=f"Download {csv_filename}",
-            data=csv_buffer,
-            file_name=csv_filename,
-            mime="text/csv",
-        )
-    st.success("All files have been converted.")
+# Display the file as a DataFrame
+if uploaded_file:
+    df = pd.read_excel(uploaded_file)
+    st.write(f"{uploaded_file.name} data:")
+    st.write(df)
+
+    csv_buffer = convert_excel_to_csv(uploaded_file)
+    csv_filename = os.path.splitext(uploaded_file.name)[0] + ".csv"
+
+    # Display the raw text data
+    st.write("Converted CSV raw text data:")
+    csv_buffer.seek(0)
+    csv_text = csv_buffer.read().decode("utf-8")
+    st.code(csv_text, language="csv")
+
+    # Check if the last row starts with ;
+    last_row = csv_text.splitlines()[-1]
+    if last_row.startswith(";"):
+        st.warning("Recheck the .XLSX file you have uploaded. Extra rows are present.")
+
+    st.success("File has been converted.")
+    st.download_button(
+        label=f"✅ Download CSV with semicolons separator",
+        data=csv_buffer,
+        file_name=csv_filename,
+        mime="text/csv",
+        type="primary",
+        use_container_width=True,
+    )
+
 else:
-    st.write("Please upload Excel files to convert.")
+    st.write("Please upload an Excel file to convert.")
