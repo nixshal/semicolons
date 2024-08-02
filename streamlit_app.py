94e84fd1c2735212a298a3031c375ec84b0dc366
Nixshal <nixshal@gmail.com>
2024-08-02 18:29:59 +0800

    Refactor Excel to CSV conversion logic in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 
 # Function to convert an Excel file to CSV with semicolon delimiter
 def convert_excel_to_csv(excel_file):
-    excel_data = pd.read_excel(excel_file).dropna(how="all")  # Remove empty rows
+    excel_data = pd.read_excel(excel_file,dtype=str).dropna(how="all")  # Remove empty rows
     csv_buffer = BytesIO()
     excel_data.to_csv(csv_buffer, sep=";", index=False)
     csv_buffer.seek(0)
@@ -20,13 +20,13 @@ uploaded_file = st.file_uploader(label="Upload Excel file after manual edits are
 
 # Display the file as a DataFrame and convert to CSV
 if uploaded_file:
-    df = pd.read_excel(uploaded_file)
+    df = pd.read_excel(uploaded_file,dtype=str)
     st.write(f"{uploaded_file.name} data:")
     st.write(df)
 
     csv_buffer = convert_excel_to_csv(uploaded_file)
     csv_text = csv_buffer.getvalue().decode("utf-8")
-    csv_filename = uploaded_file.name.replace(".xlsx", ".csv")
+    csv_filename = "cleaned_" + uploaded_file.name.replace(".xlsx", ".csv")
 
     # Display the raw text data
     st.write("Converted CSV raw text data:")
