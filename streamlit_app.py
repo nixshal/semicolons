fa77cab025465a7de2ecab4408b90364aa474f39
Nixshal <nixshal@gmail.com>
2024-08-02 17:56:29 +0800

    chore: Refactor Excel to CSV conversion logic in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import pandas as pd
-import os
 from io import BytesIO
 
 
 # Function to convert an Excel file to CSV with semicolon delimiter
 def convert_excel_to_csv(excel_file):
-    excel_data = pd.read_excel(excel_file)
-    excel_data.dropna(how="all", inplace=True)  # Remove empty rows
+    excel_data = pd.read_excel(excel_file).dropna(how="all")  # Remove empty rows
     csv_buffer = BytesIO()
     excel_data.to_csv(csv_buffer, sep=";", index=False)
     csv_buffer.seek(0)
@@ -20,29 +18,27 @@ st.subheader("Excel to CSV (semicolon delimiter)")
 # File uploader for a single file
 uploaded_file = st.file_uploader(label="Upload Excel file", type="xlsx")
 
-# Display the file as a DataFrame
+# Display the file as a DataFrame and convert to CSV
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
     st.write(f"{uploaded_file.name} data:")
     st.write(df)
 
     csv_buffer = convert_excel_to_csv(uploaded_file)
-    csv_filename = os.path.splitext(uploaded_file.name)[0] + ".csv"
+    csv_text = csv_buffer.getvalue().decode("utf-8")
+    csv_filename = uploaded_file.name.replace(".xlsx", ".csv")
 
     # Display the raw text data
     st.write("Converted CSV raw text data:")
-    csv_buffer.seek(0)
-    csv_text = csv_buffer.read().decode("utf-8")
     st.code(csv_text, language="csv")
 
     # Check if the last row starts with ;
-    last_row = csv_text.splitlines()[-1]
-    if last_row.startswith(";"):
+    if csv_text.splitlines()[-1].startswith(";"):
         st.warning("Recheck the .XLSX file you have uploaded. Extra rows are present.")
 
     st.success("File has been converted.")
     st.download_button(
-        label=f"✅ Download CSV with semicolons separator",
+        label="✅ Download CSV with semicolons separator",
         data=csv_buffer,
         file_name=csv_filename,
         mime="text/csv",
